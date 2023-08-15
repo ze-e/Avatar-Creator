bbba39889af7009ea92c0b5f7c22e1c7d0470b24
ze-e <white.noise616@gmail.com>
2023-08-15 14:12:00 -0600

    added modal close functionality

--- src/components/Modal/ModalContainer.js
+++ src/components/Modal/ModalContainer.js
@@ -1,11 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ModalContext } from 'contexts/ModalContext'
 
 export default function ModalContainer () {
   const { modalContent, modalOpen, setModalOpen, modalStyle } = useContext(ModalContext)
 
+  function clickClose (e) {
+    e.stopPropagation()
+    if (e.target.matches('.modalContainer')) setModalOpen(false)
+  }
+  function escapeClose (e) {
+    if (e.key === 'Escape') setModalOpen(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keyup', escapeClose, false)
+    return () => {
+      document.removeEventListener('keyup', escapeClose, false)
+    }
+  }, [])
+
   return modalOpen ? (
-    <div className="modalContainer visible">
+    <div className="modalContainer visible" onClick={(e) => clickClose(e)}>
       <div className={`modalContainer__inner ${modalStyle === 'small' && 'modalContainer__inner--small'}`}>
         <div className="modalContainer__content">{modalContent}</div>
         <button
