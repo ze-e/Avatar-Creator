facaada23d026deb8c686a6cdea601c4717edfcd
ze-e <white.noise616@gmail.com>
2023-11-04 16:44:12 -0600

    fixed api env

--- src/api/config/index.js
+++ src/api/config/index.js
@@ -1,2 +1,1 @@
-// export default { backendURL: 'https://avatar-creator-api.onrender.com' }
-export default { backendURL: 'http://localhost:3000' }
+export default { backendURL: process.env.BACKEND_URL || 'http://localhost:3000' }
