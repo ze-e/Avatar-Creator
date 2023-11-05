8683c9b613b434814bb252ef2a7e6c9b0ee1ad2a
ze-e <white.noise616@gmail.com>
2023-11-04 20:50:27 -0600

    remove hardcode url

--- src/api/config/index.js
+++ src/api/config/index.js
@@ -1,1 +1,2 @@
-export default { backendURL: 'https://avatar-creator-api.onrender.com' }
+export default { backendURL: process.env.BACKEND_URL }
+// export default { backendURL: 'https://avatar-creator-api.onrender.com' }
